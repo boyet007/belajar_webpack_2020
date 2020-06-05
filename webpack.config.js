33b8e62eadfc73349f9322707b9e9d3243affdc1
Philip Loa <boyet007@gmail.com>
2020-06-05 16:00:13 +0700

    berkenalan dengan webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
+        }
+      }
+    ]
+  },
+  optimization: {
+		// We no not want to minimize our code.
+		minimize: false
+	},
+};
